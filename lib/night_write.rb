aa278aaefb0ef661b6aac53a01610e9a2c34e8c8
Molly Brown <mollbrown@gmail.com>
2016-10-19 13:34:59 -0600

    Added night read and night write as seperate files

---
+++ lib/night_write.rb
@@ -0,0 +1,57 @@
+
+require './translator.rb'
+require 'pry'
+
+# Translate English text to Braille
+class NightWriter
+
+	def initialize
+		@translation = Translator.new
+	end							
+
+	def file_reader
+		text_reader = File.open(ARGV[0], "r")
+		@incoming_text = text_reader.read
+		text_reader.close
+	end
+
+	def translate_to_braille_l1
+		@translation.translate_text_to_braille_l1(@incoming_text)
+	end	
+
+	def translate_to_braille_l2
+		@translation.translate_text_to_braille_l2(@incoming_text)
+	end	
+
+	def translate_to_braille_l3
+		@translation.translate_text_to_braille_l3(@incoming_text)
+	end
+
+	def get_braille
+		@braille = @translation.combine_all_translated_text
+	end	
+
+	def write_braille
+		braille_writer = File.open(ARGV[1], 'w')
+		braille_writer.write(@braille)
+	end
+
+	def character_counter
+		@character_count = @incoming_text.chomp.length
+	end
+
+	def output_message_to_terminal
+		puts "Created '#{ARGV[1]}' containing #{@character_count} characters."
+	end 
+
+end
+
+night_writer = NightWriter.new
+night_writer.file_reader
+night_writer.character_counter
+night_writer.output_message_to_terminal
+night_writer.translate_to_braille_l1
+night_writer.translate_to_braille_l2
+night_writer.translate_to_braille_l3
+night_writer.get_braille
+night_writer.write_braille
